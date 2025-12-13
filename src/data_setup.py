18d0034ae7ff2a8299a9e4a518f7c8872ef7ece5
Furkan Kınataş <ahmetfrknknts@gmail.com>
2025-12-13 23:13:00 +0300

    Implemented training pipeline for resnet50 and vit

---
+++ src/data_setup.py
@@ -0,0 +1,31 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+
+def create_dataloaders(data_dir, batch_size=32):
+    """
+    Loads and splits the dataset. Returns the dataloaders
+    """
+
+    data_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    full_dataset = datasets.ImageFolder(root=data_dir, transform=data_transform)
+
+    class_names = full_dataset.classes
+
+    total_count = len(full_dataset)
+    train_count = int(0.8 * total_count)
+    val_count = int(0.1 * total_count)
+    test_count = val_count
+
+    train_data, val_data, test_data = random_split(full_dataset, [train_count, val_count, test_count])
+
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+
+    return train_loader, val_loader, test_loader, class_names
---
+++ src/model_setup.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+from torchvision import models
+from torchvision.models import ResNet50_Weights, ViT_B_16_Weights
+
+def get_model(model_name, num_classes, device):
+    """
+    Gets the wanted model from torch and returns it
+    """
+
+    if model_name == "resnet50":
+        weights = ResNet50_Weights.DEFAULT
+        model = models.resnet50(weights=weights)
+
+        num_features = model.fc.in_features
+        model.fc = nn.Linear(num_features, num_classes)
+
+    elif model_name == "vit_b_16":
+        weights = ViT_B_16_Weights.DEFAULT
+        model = models.vit_b_16(weights=weights)
+
+        num_features = model.heads.head.in_features
+        model.heads.head = nn.Linear(num_features, num_classes)
+
+    else:
+        print("Error. Wrong model name")
+        return None
+    
+    return model.to(device)
