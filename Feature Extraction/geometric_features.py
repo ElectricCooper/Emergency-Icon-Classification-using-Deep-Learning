b9c7e4ab38e42f9ea02f830a6e95d142ac541100
ElectricCooper <ledret.thomas@gmail.com>
2025-12-25 20:09:51 +0100

    Corrected geometric feature logic according to binary outpput format of prepared data (overcomplicated things and forgot solidity)

--- Feature Extraction/geometric_features.py
+++ Feature Extraction/geometric_features.py
@@ -16,57 +16,23 @@ class GeometricFeatures:
                         cv2.RETR_EXTERNAL,
                         cv2.CHAIN_APPROX_SIMPLE
                         )
-        perimeter = 0.0
-        for contour in contours:
-            perimeter += cv2.arcLength(contour, True)
-        return perimeter
+        return sum(cv2.arcLength(cnt, True) for cnt in contours)
 
     @staticmethod
-    def compute_area(binary_img):
-        """Compute area using morphological operations"""
-        kernel_size = 100
-        kernel = cv2.getStructuringElement(
-                    cv2.MORPH_ELLIPSE,
-                    (kernel_size, kernel_size)
-                    )
-
-        # Adding padding
-        padding = kernel_size // 2
-        padded_img = cv2.copyMakeBorder(
-                        binary_img,
-                        padding,
-                        padding,
-                        padding,
-                        padding,
-                        cv2.BORDER_CONSTANT,
-                        value=255)
-
-        # Morphological operations
-        eroded_img = cv2.erode(padded_img, kernel)
-        dilated_img = cv2.dilate(eroded_img, kernel)
-
-        # Removing padding
-        binary_img = dilated_img[padding:padding + binary_img.shape[0],
-                                 padding:padding + binary_img.shape[1]]
-
-        # Invert and count
-        binary_img = cv2.bitwise_not(binary_img)
-        area = cv2.countNonZero(binary_img)
-
-        return area
+    def compute_area(image):
+        """Compute area of binary image"""
+        return float(cv2.countNonZero(image))
 
     @staticmethod
     def compute_compactness(perimeter, area):
         """Calculate compactness = (area * 4 * pi) / (perimeter^2)"""
-        if perimeter == 0:
+        if perimeter <= 0:
             return 0.0
-        return (area * 4 * np.pi) / (perimeter * perimeter)
+        return (area * 4 * np.pi) / (perimeter ** 2)
 
     @staticmethod
     def calculate_convex_area(image):
         """Calculate area using convex hull of the largest contour"""
-
-        # Find contours
         contours, _ = cv2.findContours(
                         image,
                         cv2.RETR_EXTERNAL,
@@ -75,18 +41,12 @@ class GeometricFeatures:
         if not contours:
             return 0.0
 
-        # get all points
-        all_points = []
-        for contour in contours:
-            all_points.extend(contour.reshape(-1, 2))
-
-        if len(all_points) < 3:
-            return 0.0
-
-        all_points = np.array(all_points)
-
-        # Compute convex hull
+        # Combine all contours points to handle multiple part drawings
+        all_points = np.vstack(contours)
         hull = cv2.convexHull(all_points)
-        area = cv2.contourArea(hull)
+        convex_area = cv2.contourArea(hull)
+        area = GeometricFeatures.compute_area(image)
 
-        return area
+        solidity = area / convex_area if convex_area > 0 else 0
+
+        return {"convex_area": convex_area, "solidity": solidity}
